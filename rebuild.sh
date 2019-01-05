1e437dbcccd1b0178fd0481079b8236ee30e6143
Frank Zhao <frank.zhao@section6.nz>
2019-01-05 13:12:31 +1300

    rebuilding site Sat Jan  5 13:12:31 NZDT 2019

---
+++ rebuild.sh
@@ -0,0 +1,14 @@
+yes | cp -rf ../blog/public/* .
+
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
